ec9517e616cb8a5b32126cf28e68e3c65685f401
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2021-06-18 22:28:07 -0700

    adding a comment

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
+  //my first commit
   return <h2>reminder project setup</h2>;
 }
 
